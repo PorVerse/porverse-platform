d312f3f78d48b1607314d004c05e35673da60de3
PorVerse <porverseofficial@gmail.com>
2025-08-09 20:18:29 +0300

    Add missing UI components

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-/** @type {import('postcss-load-config').Config} */
+// postcss.config.js
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
 
-export default config;
+export default config
